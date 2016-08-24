1db8c841c2beef2c564cde2a92fb1019cac1d31c
yamon <ngockien16@gmail.com>
2016-08-24 14:22:29 +0700

    fix conflict

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def login_admin
-<<<<<<< HEAD
-    # admin_root_admin_path
-    # binding.pry
-=======
-    
->>>>>>> 7ced8930527a83f4c6800131a82df8d213bdedaa
+    admin_root_admin_path
+    binding.pry
   end
   def create
   end
