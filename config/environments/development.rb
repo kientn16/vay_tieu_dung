f2d35ab37c84dc8bfe10b8884bc0c25127d7890d
yamon <ngockien16@gmail.com>
2016-08-24 10:47:12 +0700

    Add layout client

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  config.active_support.deprecation = :log1
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
