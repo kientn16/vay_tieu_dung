4577d2b7c6b2fb61862920243af3d6e723cfbab8
yamon <ngockien16@gmail.com>
2016-09-16 16:25:50 +0700

    Commit Environments varb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,8 +50,8 @@ Rails.application.configure do
       :port => 587,
       :domain => "gmail.com",
       :authentication => 'plain',
-      :user_name => "",
-      :password => "",
+      :user_name => ENV["GMAIL_USERNAME"],
+      :password => ENV["GMAIL_PASSWORD"],
       :enable_starttls_auto => true
   }
 
